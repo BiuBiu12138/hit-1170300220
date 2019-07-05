358e7979d6465cbedcfb97cc96947d3fab73cb7c
1170300323 <48124708+1170300323@users.noreply.github.com>
2019-07-05 14:02:28 +0800

    Create HelloA3_1170300323_2.c
    the third part of A3, I follow-up to my classmate's feedback

---
+++ HelloA3_1170300323_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int i = 20;
+  printf("this is another request pulled by 1170300323\n");
+  printf("count from 20 to 0");
+  for(i = 20; i >= 0; i--){
+    printf(i);
+    printf("\n");
+  }
+}
